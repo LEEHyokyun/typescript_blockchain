1d3a3181026894f9d2d9249411525337ad7cba54
hyokyun <gyrbs22@gmail.com>
2021-09-16 21:47:39 +0900

    static method

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+import * as CryptoJS from "crypto-js"
+
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockhash = (index:number, previousHash:string, timestamp:number, data:string) => {
+        CryptoJS.SHA256(index + previousHash + timestamp + data).toString()
+    }
 
     constructor(
         index: number,
@@ -19,13 +25,15 @@ class Block {
         this.timestamp = timestamp;
     }
 }
+//static method
+Block.calculateBlockhash
 
 // declare : type
 const firstBlock : Block = new Block(0, "20200916", "", "FIRST", 0);
 
 // declare : type(array)
 // array only allows the same class as Block.
-let blockchain : [Block] = [firstBlock]
+let blockchain : Block[] = [firstBlock]
 
 // check the log
 console.log(blockchain)
